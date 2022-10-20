a36a1fc3da4480bf9496ce86820d427471d151e1
Serge <serge.andre.masson@gmail.com>
2022-10-20 09:03:48 +0200

    GCS access test

--- app.py
+++ app.py
@@ -1,4 +1,18 @@
+from io import BytesIO
 import streamlit as st
+from google.cloud import storage
+import pandas as pd
 
+
+client = storage.Client()
+
+BUCKET_NAME = "artifacts.test-git-cloud-run-storage.appspot.com"
+BUCKET = client.bucket(BUCKET_NAME)
+DATA_ACCESS = 'google clood'
 
 st.write("hello streamlit!")
+path = "data/famille.csv"
+csv_path  = BytesIO(BUCKET.blob(path).download_as_bytes())
+famille = pd.read_csv(csv_path)
+st.write(famille)
+
